dd852a2eaddd54195e1c83a37663f4e3b652e9aa
snkrfrkr <carlos.perry.rbg@gmail.com>
2018-04-30 15:56:48 +0200

    mysql added

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import time, bcrypt
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
 Bootstrap(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'service'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'schubert'
+app.config['MYSQL_DATABASE_DB'] = 'gripson_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+mysql.connect()
+if mysql:
+    print ("OK")
+
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
-            return render_template("index.html")
+        try:
+            cur = mysql.connect().cursor()
+            sql = "SELECT text FROM gripson_t"
+            cur.execute(sql)
+            data = cur.fetchall()
+            return str(data)            
+            #return render_template("index.html")
+        except:
+            print("NOK")
+        
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
