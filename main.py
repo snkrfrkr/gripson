53b1df9e06f368d068e715bd42a65efb268c0cac
snkrfrkr <carlos.perry.rbg@gmail.com>
2018-05-11 16:37:34 +0200

    guter stand

--- main.py
+++ main.py
@@ -21,12 +21,24 @@ if mysql:
 def index():
     if request.method == 'GET':
         try:
-            cur = mysql.connect().cursor()
-            sql = "SELECT text FROM gripson_t"
-            cur.execute(sql)
-            data = cur.fetchall()
-            return str(data)            
-            #return render_template("index.html")
+            cur1 = mysql.connect().cursor()
+            sql_id = "SELECT id FROM gripson_t"
+            cur1.execute(sql_id)
+            id_t = cur1.fetchall()
+            
+            cur2 = mysql.connect().cursor()
+            sql_text = "SELECT text FROM gripson_t"
+            cur2.execute(sql_text)
+            text_t = cur2.fetchall()
+
+            cur3 = mysql.connect().cursor()
+            sql_desc = "SELECT description FROM gripson_t"
+            cur3.execute(sql_desc)
+            desc_t = cur3.fetchall()
+
+            db_len = (len(id_t))
+
+            return render_template("index.html", db_len=db_len, idt=id_t, text=text_t, desc=desc_t)
         except:
             print("NOK")
         
